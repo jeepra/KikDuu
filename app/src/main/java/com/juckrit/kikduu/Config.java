da1813cfb68a013e76136570167510e2a2ff2897
Juckrit  Youyen <juckrit@Juckrits-MacBook-Pro.local>
2016-07-02 08:56:53 +0700

    Initial commit

---
+++ app/src/main/java/com/juckrit/kikduu/Config.java
@@ -0,0 +1,12 @@
+package com.juckrit.kikduu;
+
+/**
+ * Created by juckrit on 5/3/2016 AD.
+ */
+public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBnYVJk2jWF7Iw_Sp6Q5rxE1TOvOv4Og_U";
+}
